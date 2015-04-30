25051e4e9557715666e8cb859e6571a45f723b5d
s134319 <s134319@hftl.de>
2015-04-30 19:52:29 +0200

    Sprint #1: item #1.2.3 and item #1.3.3
    Sprint #1: item #1.2.3 and item #1.3.3
    Finished new drafts of the mockup.
    
    The following sprint items are included:
    - #1.2.3 Menü -> Mockup
    - #1.3.3 Lokale auflisten-> Mockup
    
    Also included but not as a final version:
    - #x.4.x Lokal Details -> Mockup

--- p05-integration/mockup/js/veganguide.js
+++ p05-integration/mockup/js/veganguide.js
@@ -5,6 +5,38 @@
  * Event: on mobileinit 
  */
 $(document).bind("mobileinit", function(){
-  $.mobile.defaultPageTransition = "slide"; /* set default transition: slide */
+
+	/**
+	 * Settings for all pages 
+	 */
+	$.mobile.defaultPageTransition = "slide"; /* set default transition: slide */
+
+	/*
+	 * Page: place
+	 * Event: onClick Comments-Expand button
+	 */
+	$(document).on('vclick', '#place #buttonComments', function(){ 
+		/* state of commment list: collapsed? */
+		var isCollapsed = $( "#place #comments" ).collapsible( "option", "collapsed" );
+		if(isCollapsed) { // is collapsed
+	    	$( "#place #comments" ).collapsible( "expand" ); // expand list
+	    	$( "#place #buttonComments").buttonMarkup({ icon: "minus" }); // show minus as icon in button
+	  } else { // is expanded
+	  		$( "#place #comments" ).collapsible( "collapse" ); // collapse list
+	  		$( "#place #buttonComments").buttonMarkup({ icon: "plus" }); // show plus as icon in button
+	  	}
+	});
+
 });
 
+/**
+ * Page: all
+ * Event: on ready
+ *
+ */
+$(document).ready(function() {
+	//$("#lightGallery").lightGallery(); 
+	$("#lightGallery").lightGallery({ // load light gallery
+		thumbnail            : false, // dont show thumbnails
+	});
+});
