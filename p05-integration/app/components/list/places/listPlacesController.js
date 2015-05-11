d24374d43ae63b839672f7ae6e10971e15fb894c
s134319 <s134319@hftl.de>
2015-05-11 19:15:04 +0200

    Sprint 1: Frontend: API-Zugriff Bugfix

--- p05-integration/app/components/list/places/listPlacesController.js
+++ p05-integration/app/components/list/places/listPlacesController.js
@@ -21,11 +21,3 @@ ngApp.controller('listPlacesController', ['$scope', '$filter', '$routeParams', '
 	$scope.placeList = new PlaceList($routeParams.country, $routeParams.city); // get the PlaceList model
 	$scope.countryId = $routeParams.country;
 }]);
-
-/*
-ngApp.controller('listPlacesController',function($scope, $http, $filter, $routeParams){
-	$http.get('api/JSON_Dummies/Lokale_Leipzig.json').success (function(result){ // Load Places using a API-call wich returns JSOn data
-		$scope.places = result; // put list of Places into scope
-	});
-});
-*/
