8263a38d15ff8c93ef50dc7b3d668ecaaf4063d5
s134319 <s134319@hftl.de>
2015-06-21 14:08:36 +0200

    Frontend: Bugfix Umlaute und Slashes
    Durch HTML-Entitites bei Umlauten konnten nicht nach Umlauten gefiltert
    werden. Außerdem werden unnötige Slashes enfernt.
    
    FRONTEND: Escape-Zeichen bei Hochkommas entfernen #10

--- p05-integration/mobileapp/www/app/shared/api/noFaultyFilter.js
+++ p05-integration/mobileapp/www/app/shared/api/noFaultyFilter.js
@@ -4,16 +4,20 @@
  * @name app.api.noFaulty
  * @description
  * Filter to delete entries in an array of objects by a key when they seems to be faulty.
+ * Also parse input from HTML (the entities).
+ * And also strip slashes.
  * 
  * @param {object} collection - data which gets manipulated
  * @param {string} key - only look in this property of any child  object
  */
-angular.module('mvg.api').filter('noFaulty', function () {
+angular.module('mvg.api').filter('noFaulty', function ($sce) {
 	return function(collection, keyname) {
 		var output = [];
 		angular.forEach(collection, function(item) { // loop any element in the input array
 			var key = item[keyname]; // get value of the observed key
-			if(key.charAt(0) !== "!" && key !== "") { // first char is not a '!' and not empty
+			if (key.charAt(0) !== "!" && key !== "") { // first char is not a '!' and not empty
+			    item[keyname] = angular.element('<textarea />').html(item[keyname]).text(); // make sure html entitites are decoded
+			    item[keyname] = item[keyname].replace(new RegExp("\\\\", "g"), "");
 				output.push(item); // push to output array
 			}
 		});
